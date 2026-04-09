4f38ee9e2224d079c31e36930ef02f229d22e4d5
Leonardo Coradi <clashonpq@gmail.com>
2026-04-09 15:01:44 -0300

    teetetteet

--- store/useClientesStore.ts
+++ store/useClientesStore.ts
@@ -42,13 +42,7 @@ export const useClientesStore = create<ClientesStore>()((set) => ({
 
     const { data: insertedData, error: insertError } = await supabase
       .from('clientes')
-      .insert([{ 
-        nome: data.nome,
-        telefone: data.telefone,
-        notas: data.notas,
-        ultimaVisita: data.ultimaVisita,
-        user_id: userData.user.id 
-      }])
+      .insert([{ ...data, user_id: userData.user.id }])
       .select()
       .single();
 
